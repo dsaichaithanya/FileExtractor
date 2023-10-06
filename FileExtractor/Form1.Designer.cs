51cfdcf4bfd2df558183e7dcf13ebb97f361dd4f
EHSKH <dsai.chaithanya.ext@bayer.com>
2023-10-06 11:33:07 +0530

    initial commit

---
+++ FileExtractor/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿
+namespace FileExtractor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnProcess = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.label = new System.Windows.Forms.Label();
+            this.lblversion = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnProcess
+            // 
+            this.btnProcess.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcess.Location = new System.Drawing.Point(131, 173);
+            this.btnProcess.Name = "btnProcess";
+            this.btnProcess.Size = new System.Drawing.Size(170, 31);
+            this.btnProcess.TabIndex = 0;
+            this.btnProcess.Text = "Process";
+            this.btnProcess.UseVisualStyleBackColor = true;
+            this.btnProcess.Click += new System.EventHandler(this.btnProcess_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Trebuchet MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(119, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 27);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File Extractor";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.btnProcess);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.groupBox1.Location = new System.Drawing.Point(6, -1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(424, 343);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSelectFile);
+            this.groupBox2.Controls.Add(this.lblFileName);
+            this.groupBox2.Controls.Add(this.label);
+            this.groupBox2.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(10, 59);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(406, 94);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "File Options";
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.Font = new System.Drawing.Font("Trebuchet MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSelectFile.Location = new System.Drawing.Point(121, 26);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(170, 31);
+            this.btnSelectFile.TabIndex = 2;
+            this.btnSelectFile.Text = "Browse";
+            this.btnSelectFile.UseVisualStyleBackColor = true;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFileName.ForeColor = System.Drawing.Color.Maroon;
+            this.lblFileName.Location = new System.Drawing.Point(130, 67);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(0, 18);
+            this.lblFileName.TabIndex = 1;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.ForeColor = System.Drawing.Color.Black;
+            this.label.Location = new System.Drawing.Point(46, 33);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(69, 18);
+            this.label.TabIndex = 1;
+            this.label.Text = "Select File";
+            // 
+            // lblversion
+            // 
+            this.lblversion.AutoSize = true;
+            this.lblversion.Font = new System.Drawing.Font("Trebuchet MS", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblversion.ForeColor = System.Drawing.Color.Black;
+            this.lblversion.Location = new System.Drawing.Point(6, 345);
+            this.lblversion.Name = "lblversion";
+            this.lblversion.Size = new System.Drawing.Size(59, 18);
+            this.lblversion.TabIndex = 3;
+            this.lblversion.Text = "V.1.0.0.0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 365);
+            this.Controls.Add(this.lblversion);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MinimumSize = new System.Drawing.Size(453, 404);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FileExtractor";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnProcess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Label lblFileName;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Label lblversion;
+    }
+}
+
---
+++ FileExtractor/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+
+namespace FileExtractor
+{
+    public partial class Form1 : Form
+    {
+        string filepath = null;
+        DataTable dt = new DataTable();
+        string fileDate = null;     
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblversion.Text = "V.1.0.0.0";
+
+            dt.Columns.Add("Folder No");
+            dt.Columns.Add("Response Date");
+            dt.Columns.Add("Response Time");
+        }
+
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    lblFileName.Text = openFileDialog1.SafeFileName;
+                    filepath = openFileDialog1.FileName;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+
+        }
+
+        private void btnProcess_Click(object sender, EventArgs e)
+        {
+            List<string> finalData = new List<string>();
+            try
+            {
+                if (filepath != null)
+                {
+                    string[] lines = File.ReadAllLines(filepath);
+
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        if (lines[i].Contains("Order successfully created"))
+                        {
+                            string[] splitFolder = lines[i].Split(',');
+                            string folderName = splitFolder[2].ToString().Substring(0, splitFolder[2].Length - 1);
+
+                            if (lines[i - 1] != null)
+                            {
+                                string[] splitDt = lines[i - 1].Split('/');
+                                string date = splitDt[1].ToString().Substring(1, splitDt[1].Length - 2);
+                                fileDate = date;
+                                date = date.Insert(4, "/");
+                                date = date.Insert(7, "/");
+                                string time = splitDt[2].ToString().Substring(1, splitDt[2].Length - 2);
+
+                                ProcessData(folderName, date, time);
+                            }
+                        }
+                    }
+
+                    bool flag = ExportExcel();
+                    
+                    if(flag == true)
+                    {
+                        MessageBox.Show("File Exported Successfully", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error occurred while exporting", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please select the file to process", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private bool ExportExcel()
+        {
+            bool flg = false;
+
+            try
+            {
+                //Exporting to Excel
+                string folderPath = "C:\\Excel\\";
+                if (!Directory.Exists(folderPath))
+                {
+                    Directory.CreateDirectory(folderPath);
+                }
+                using (XLWorkbook wb = new XLWorkbook())
+                {
+                    wb.Worksheets.Add(dt, "LogFile" + fileDate);
+                    wb.SaveAs(folderPath + "LogFile_" + fileDate + ".xlsx");
+                }
+                flg = true;
+            }
+          
+            catch (Exception ex)
+            {
+                flg = false;
+                //MessageBox.Show(ex.Message);
+            }
+            return flg;
+        }
+
+        private void ProcessData(string folderName, string date, string time)
+        {
+            if (folderName != null && date != null && time != null)
+            {
+                dt.Rows.Add(folderName, date, time);
+            }
+            
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
